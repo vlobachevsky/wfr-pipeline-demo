7a26dcdc727725095085e39f2070a330ea238d63
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-08-11 18:49:37 +0300

    Implement qa-pipeline

--- qa-pipeline.groovy
+++ qa-pipeline.groovy
@@ -1,1 +1,23 @@
 #!/usr/bin/env groovy
+
+properties([
+  parameters([
+    booleanParam(name: 'DEPLOY_ON_AP01', defaultValue: true,
+        description: 'Deploys selected build on AP01 node.'),
+    booleanParam(name: 'DEPLOY_ON_MW01', defaultValue: true,
+        description: 'Deploys selected build on MW01 node.'),
+    booleanParam(name: 'DEPLOY_ON_MW02', defaultValue: true,
+        description: 'Deploys selected build on MW02 node.'),
+    choices(name: 'BUILD_TO_DEPLOY', defaultValue: 'TESTING\nSTAGING\nPRODUCTION',
+        description: 'Logical group of agent to run the job on. ', )
+   ])
+])
+
+
+node() {
+
+    stage('Deploy') {
+        echo "BUILD_TO_DEPLOY: $params.BUILD_TO_DEPLOY"
+    }
+
+}
