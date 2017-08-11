9ef1fba55d2a4196b01db6ca7ab8c1bf39ae36d1
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-08-11 19:06:07 +0300

    Implement qa-pipeline

--- qa-pipeline.groovy
+++ qa-pipeline.groovy
@@ -8,7 +8,7 @@ properties([
         description: 'Deploys selected build on MW01 node.'),
     booleanParam(name: 'DEPLOY_ON_MW02', defaultValue: true,
         description: 'Deploys selected build on MW02 node.'),
-    string(name: 'BUILD_TO_DEPLOY', defaultValue: '', choices: 'TESTING\nSTAGING\nPRODUCTION',
+    choice(name: 'BUILD_TO_DEPLOY', choices: ['Zeyt-XXX.zip', 'Zeyt-YYY.zip', 'Zeyt-ZZZ.zip'],
         description: 'Logical group of agent to run the job on. ', )
    ])
 ])
