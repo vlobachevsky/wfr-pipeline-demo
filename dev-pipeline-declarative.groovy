a4b0538c424a8d27f9327273c6d9bace349bd526
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-08-22 13:54:00 +0300

    Implement declarative pipeline POC

--- dev-pipeline-declarative.groovy
+++ dev-pipeline-declarative.groovy
@@ -24,7 +24,7 @@ pipeline {
         stage('Build') {
             steps {
                 checkoutSVN(svnCredentialsId, "$svnRootURL/zeyt")
-                buildZeyt
+                buildZeyt()
             }
         }
         stage('Unit Tests') {
