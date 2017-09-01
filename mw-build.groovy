2eb4a967c52b8bd56ace02ccaef151435e881c43
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-01 17:37:55 +0300

    Implement Build step in mw-build pipeline

--- mw-build.groovy
+++ mw-build.groovy
@@ -1,5 +1,8 @@
 #!groovy
 
+// Build PunchMW application
+// Requires MS Build Tools installed on slave (for Access Control)
+
 pipeline {
     agent {
         label 'pipeline'
@@ -21,8 +24,15 @@ pipeline {
                 checkoutSVN(svnCredentialsId, "$svnRootURL/PunchMW")
                 checkoutSVN(svnCredentialsId, "$svnRootURL/Documents/DevOps/Scripts/Env_Configs/$envId", 'env')
                 buildMW()
+                // Build Access Control
+                powershell(script: 'Write-Host aaa')
             }
         }
+
+        stage('Publish') {
+            echo 'Publish...'
+        }
+
     }
 
 }
