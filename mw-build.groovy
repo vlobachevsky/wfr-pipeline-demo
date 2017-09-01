88a1536a1ca3b29856b8b05d8f4e0251a826353c
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-01 17:09:49 +0300

    Initial mw-build pipeline

---
+++ mw-build.groovy
@@ -0,0 +1,28 @@
+#!groovy
+
+pipeline {
+    agent {
+        label 'pipeline'
+    }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+    }
+
+    environment {
+        svnCredentialsId = 'vital.lobachevskij-wrf-svn'
+        svnRootURL = 'svn://kap-wfr-svn.int.kronos.com'
+        env = 'maindev'
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                checkoutSVN(svnCredentialsId, "$svnRootURL/PunchMW")
+                checkoutSVN(svnCredentialsId, "$svnRootURL/Documents/DevOps/Scripts/Env_Configs/$env")
+                buildMW()
+            }
+        }
+    }
+
+}
