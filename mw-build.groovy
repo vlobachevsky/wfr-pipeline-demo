dca044595e32f903d5bcf6504fdc0008c3ebf282
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-02 17:47:28 +0300

    Implement Build step in mw-build pipeline

--- mw-build.groovy
+++ mw-build.groovy
@@ -23,10 +23,9 @@ pipeline {
         stage('Build') {
             steps {
                 // Checkout PunchMW repo
-//                checkoutSVN(svnCredentialsId, "$svnRootURL/PunchMW")
                 checkoutSVN(svnCredentialsId, "svn://kap-wfr-svn.int.kronos.com/PunchMW")
                 // Checkout environment config files
-//                checkoutSVN(svnCredentialsId, "$svnRootURL/Documents/DevOps/Scripts/Env_Configs/$envId", 'env')
+                checkoutSVN(svnCredentialsId, "$svnRootURL/Documents/DevOps/Scripts/Env_Configs/$envId", 'env')
 //                buildMW()
                 // Build Access Control
 //                powershell 'Write-Output "Hello, World!"'
