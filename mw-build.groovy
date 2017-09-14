0f0a2e5b08428770e3527e07722de4a0cb9867c9
Lobachevskij, Vital <Vital.Lobachevskij@Kronos.com>
2017-09-14 11:37:38 +0300

    Implement Send Notifications

--- mw-build.groovy
+++ mw-build.groovy
@@ -95,9 +95,7 @@ pipeline {
                 subject: "${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - ${env.BUILD_STATUS}!"
             )
 */
-            sendMail(
-                subject: 'Test'
-            )
+            sendMail()
         }
     }
 
